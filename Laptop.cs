39889b2210928542596c180da6f773e99c0b059e
PC <PC@DESKTOP-S31ELKF>
2024-05-14 02:37:19 +0300

    Add project files.

---
+++ Laptop.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Ivv
+{
+    public class Laptop
+    {
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int YearProduce { get; set; }
+
+        public Laptop(string name, string brand, string model, int yearProduce)
+        {
+            Name = name;
+            Brand = brand;
+            Model = model;
+            YearProduce = yearProduce;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Brand: {Brand}, Model: {Model}, Year Produced: {YearProduce}";
+        }
+    }
+
+
+
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ivv
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<Laptop> laptops = new List<Laptop>()
+        {
+            new Laptop("Laptop1", "LENOVO", "Model1", 2020),
+            new Laptop("Laptop2", "DELL", "Model2", 2021),
+            new Laptop("Laptop3", "ACER", "Model3", 2022),
+            new Laptop("Laptop4", "HP", "Model4", 2023),
+            new Laptop("Laptop5", "ASUS", "Model5", 2024)
+        };
+
+            // Изведете всички компютри в нисходящ ред годината на производство
+            var orderedLaptops = laptops.OrderByDescending(l => l.YearProduce);
+            foreach (var laptop in orderedLaptops)
+            {
+                Console.WriteLine(laptop);
+            }
+
+            // Изведете всички превозни средства, които са марка “LENOVO”, “DELL” или „ACER”
+            var selectedBrands = new List<string> { "LENOVO", "DELL", "ACER" };
+            var laptopsWithSelectedBrands = laptops.Where(l => selectedBrands.Contains(l.Brand));
+            foreach (var laptop in laptopsWithSelectedBrands)
+            {
+                Console.WriteLine(laptop);
+            }
+
+            // Изведете тези лаптопи, които са произведени в периода между 2020-2024-тата година
+            var laptopsProducedBetween2020And2024 = laptops.Where(l => l.YearProduce >= 2020 && l.YearProduce <= 2024);
+            foreach (var laptop in laptopsProducedBetween2020And2024)
+            {
+                Console.WriteLine(laptop);
+            }
+        }
+    }
+}
+
